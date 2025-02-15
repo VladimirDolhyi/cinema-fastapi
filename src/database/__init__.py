181b896b9909e4d55d4fbf47ea91e371e39a5385
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2025-02-15 23:59:28 +0200

    database/init & database/models/base

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,7 +1,14 @@
-__all__ = ("Base", "UserGroupEnum", "GenderEnum", "UserGroup", "User")
-
 from .models.base import Base
 from .models.accounts import (
-    UserGroupEnum, GenderEnum, UserGroup, User
+    UserGroupEnum,
+    GenderEnum,
+    UserGroup,
+    User,
+    UserProfile,
+    ActivationToken,
+    PasswordResetToken,
+    RefreshToken
 )
-from .database import get_sqlite_db, get_sqlite_db_contextmanager, reset_sqlite_database
+from .database import get_db, get_db_contextmanager, reset_database
+
+from .validators import accounts as accounts_validators
--- src/database/models/base.py
+++ src/database/models/base.py
@@ -5,7 +5,3 @@ class Base(DeclarativeBase):
     @classmethod
     def default_order_by(cls):
         return None
-
-
-# class Base(DeclarativeBase):
-#     pass

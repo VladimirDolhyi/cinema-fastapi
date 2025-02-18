c4e14c33c5bae8bae8303ea0b82503bf998e89a1
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2025-02-18 04:35:39 +0200

    add schemas for profiles

---
+++ src/schemas/__init__.py
@@ -0,0 +1,14 @@
+from .accounts import (
+    UserRegistrationRequestSchema,
+    UserRegistrationResponseSchema,
+    UserActivationRequestSchema,
+    MessageResponseSchema,
+    UserLoginRequestSchema,
+    UserLoginResponseSchema,
+    PasswordResetRequestSchema,
+    PasswordResetCompleteRequestSchema,
+    PasswordChangeRequestSchema,
+    TokenRefreshRequestSchema,
+    TokenRefreshResponseSchema
+)
+from .profiles import ProfileRequestSchema, ProfileResponseSchema

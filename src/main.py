48e063fc11529a69e78470bc9db9f639d3d479f5
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2025-03-05 12:52:24 +0200

    add carts_router

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
 
-from src.routers import accounts_router, profiles_router, movies_router
+from src.routers import accounts_router, profiles_router, movies_router, carts_router
 
 app = FastAPI()
 
 app.include_router(accounts_router, prefix="/accounts", tags=["accounts"])
 app.include_router(profiles_router, prefix="/profiles", tags=["profiles"])
 app.include_router(movies_router, prefix="/movies", tags=["movies"])
+app.include_router(carts_router, prefix="/carts", tags=["carts"])
 
 
 if __name__ == "__main__":
